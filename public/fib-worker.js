42f3c08cdfd79c39976031ee44e98368be2a9419
minjie0501 <minjie.fan@icloud.com>
2022-03-20 15:10:08 +0100

    clean up

--- public/fib-worker.js
+++ public/fib-worker.js
@@ -1,28 +1,20 @@
-import _ from "lodash";
 import { Canvg, presets } from "canvg";
-import DOMParser from 'xmldom'
-
-
-const fib = (n) => (n < 2 ? n : fib(n - 1) + fib(n - 2));
+import DOMParser from "xmldom";
 
 onmessage = async (e) => {
-  // add two build together in package json
-  const { num, butterfly, offscreenCanvas } = e.data;
-  console.log("isitbvu")
+
+  // NOTE: dummy computationally expensive function
+  // for (let i = 0; i < 10000000000; i++) {}
+
+  const { butterfly, offscreenCanvas } = e.data;
+  console.log("trest");
   offscreenCanvas.height = 500;
   offscreenCanvas.width = 500;
-  const ctx = offscreenCanvas.getContext('2d')
+  const ctx = offscreenCanvas.getContext("2d");
   const v = await Canvg.fromString(ctx, butterfly, presets.offscreen(DOMParser));
   console.log(v.document);
-  await v.render()
-  v.start()
+  await v.render();
+  v.start();
 
-
-
-  const startTime = new Date().getTime();
-  const fibNum = fib(num);
-  postMessage({
-    fibNum,
-    time: new Date().getTime() - startTime,
-  });
+  postMessage({});
 };
